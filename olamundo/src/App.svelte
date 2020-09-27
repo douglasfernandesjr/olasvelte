a2e57166b03e1508253dd56c9626bb1755d67a70
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2020-09-26 21:08:02 -0300

    subir o que está no local

---
+++ olamundo/src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import Polaroid from './components/Polaroid.svelte';
+	export let name;
+
+	//https://svelte.dev/tutorial/reactive-assignments
+
+	let imgUrl = "https://1.bp.blogspot.com/-JSl_9choHKI/XOKiGIg97iI/AAAAAAAA5xc/nOBSjznhpmscNKAGVtTROq9Tji5Q8mJKwCLcBGAs/s1600/unnamed%2B-%2B2019-05-20T094752.071.jpg"
+
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<input bind:value={name}/>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Polaroid  url={imgUrl} alt="Texto adicional" >
+		<h2>Texto adicional</h2>
+		<h4>Texto adicional h4</h4>
+	</Polaroid>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
