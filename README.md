8951b1884f702397d651f1c719ab7342ccdc162e
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2021-01-03 14:58:59 -0300

    adicionar projeto de exemplo padrão

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # olasvelte
 Olá, Svelte!
+
+Projeto com alguns exemplos de código usando Svelte.
+
+Aprenda mais aqui [https://svelte.dev](https://svelte.dev)
+
+O que precisa para começar.
+- Node.js
+- VS Code
+ - E a extensão [Svelte for VS Code](https://marketplace.visualstudio.com/items?itemName=svelte.svelte-vscode)
+
+ Próximos Passos:
+ - Fazer o [tutorial completo](https://svelte.dev/tutorial/basics).
+ - Leia a [documentação](https://svelte.dev/docs) para entender melhor.
+ - E veja [estes exemplos](https://svelte.dev/examples#hello-world). 
---
+++ exemploApp/src/components/Polaroid.svelte
@@ -0,0 +1,85 @@
+<script>
+    import { onMount, beforeUpdate, afterUpdate } from "svelte";
+  
+    /** propriedade url */
+    export let src;
+  
+    /** Propriedade alt */
+    export let alt;
+
+    let turnLeft = false;
+
+    onMount(() => {
+      console.log("the component has mounted");
+    });
+  
+    beforeUpdate(() => {
+      console.log("the component is about to update");
+    });
+  
+    afterUpdate(() => {
+      console.log("the component just updated");
+    });
+  
+    function photoClick() {
+      turnLeft = !turnLeft;
+    }
+  </script>
+  
+  <style>
+    .polaroid {
+      max-width: 300px;
+      background: #fff;
+      display: inline-block;
+      margin: 0 0 27px 30px;
+      width: auto;
+      padding: 10px 10px 15px;
+      text-align: center;
+      font-family: "Marker Felt", sans-serif;
+      text-decoration: none;
+      color: #333;
+      font-size: 18px;
+      box-shadow: 0 3px 6px rgba(0, 0, 0, 0.25);
+      transform: rotate(-3deg);
+      transition: 0.5s;
+    }
+  
+    .polaroid.right {
+      transform: rotate(3deg);
+    }
+  
+    .polaroid:hover {
+      transition: 0.5s;
+      transform: rotate(0deg);
+      max-width: 325px;
+      margin: 0 0 0 10px;
+    }
+  
+    .polaroid img {
+      max-width: 100%;
+    }
+  </style>
+  
+<!--
+  @component
+  Here's some documentation for this component.
+  It will show up on hover.
+  
+  - You can use markdown here.
+  - You can also use code blocks here.
+  - Usage:
+    ```tsx
+    <Polaroid src={src} alt={alt}>
+      ```
+-->
+
+  <div class="polaroid" class:left={turnLeft} class:right={!turnLeft} on:click={photoClick}>
+    <img {src} {alt} />
+    <slot>
+      <h3>{alt}</h3>
+    </slot>
+    {#if turnLeft}
+      <div>left</div>
+    {/if}
+  </div>
+  
